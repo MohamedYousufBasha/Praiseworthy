d3abda6cd6d1e6af9deaa0b61135529755627348
mbasha1 <MahamedYousuf_Basha@Intuit.com>
2017-11-16 19:14:15 +0530

    Intial Commit

---
+++ src/main/java/com/ByGodsGrace/Automation/Praiseworthy/App.java
@@ -0,0 +1,13 @@
+package com.ByGodsGrace.Automation.Praiseworthy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
